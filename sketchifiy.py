42f680535f2127d1b45a898f362714469c60681e
Bart Heerkens <bartheerkens@Barts-MacBook-Pro.local>
2021-04-20 17:02:22 +0200

    Initial Commit Streamlit App

---
+++ sketchifiy.py
@@ -0,0 +1,24 @@
+""" A streamlit app that transfers photos to sketches"""
+
+import cv2
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+st.write("""
+         ## This Web App turns a color jpg into a sketch.
+         \nGo Ahead and try:
+         """
+         )
+
+image = st.file_uploader("Upload an Image", type=['jpg', 'jpeg'])
+if image:
+    st.image(image)
+    img = Image.open(image)
+    img_gray = cv2.cvtColor(np.float32(img), cv2.COLOR_RGB2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (21, 21), 0, 0)
+    img_blend = cv2.divide(img_gray, img_blur, scale=256)
+    sketch = Image.fromarray(img_blend)
+    if sketch.mode != "RGB":
+        sketch = sketch.convert("RGB")
+    st.image(sketch)
